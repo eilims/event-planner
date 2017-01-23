c9b2bda002d3d039179d7c1da5432c9148b09540
Daniel Benusovich <dbenusovich@gmail.com>
2017-01-22 17:53:25 -0800

    successfully add members, no logging in yet

--- src/main/java/com/eventplanner/domain/EventMember.java
+++ src/main/java/com/eventplanner/domain/EventMember.java
@@ -5,20 +5,23 @@
  */
 package com.eventplanner.domain;
 
-import java.io.Serializable;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  *
  * @author DanielB
  */
 @Entity
-public class EventMember implements Serializable {
+public class EventMember implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -27,48 +30,116 @@ public class EventMember implements Serializable {
     @Column
     private String username;
 
-    @ElementCollection
-    private List<Integer> eventIdList;
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private boolean enabled;
+
+    @Column
+    private boolean accountNonExpired;
+
+    @Column
+    private boolean accountNonLocked;
+
+    @Column
+    private boolean credentialsNonExpired;
 
     @ElementCollection
-    private List<Integer> groupIdList;
+    private Set<GrantedAuthority> authorities;
 
-    public EventMember(String username) {
+    public EventMember(String username, String password, String email) {
         this.username = username;
-        eventIdList = null;
-        groupIdList = null;
-    }
-    
-    protected EventMember(){
-        
+        this.password = password;
+        this.email = email;
+        this.enabled = true;
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.authorities  = new HashSet();
+        authorities.add(new SimpleGrantedAuthority("USER"));
     }
 
+    protected EventMember() {
+
+    }
+
+    @Override
+    public Set<GrantedAuthority> getAuthorities() {
+        return authorities; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPassword() {
+        return password; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
     public String getUsername() {
-        return username;
+        return username; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled; //To change body of generated methods, choose Tools | Templates.
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public void setAuthorities(Set<GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Integer getId() {
         return id;
-    }
-
-    public List<Integer> getEventIdList() {
-        return eventIdList;
-    }
-
-    public void setEventIdList(List<Integer> eventIdList) {
-        this.eventIdList = eventIdList;
-    }
-
-    public List<Integer> getGroupIdList() {
-        return groupIdList;
-    }
-
-    public void setGroupIdList(List<Integer> groupIdList) {
-        this.groupIdList = groupIdList;
     }
 
 }
